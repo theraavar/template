e11bfd929e8a9acad0805a0accf1ae0edc72094e
ruyla <ruyla@proton.me>
2023-08-01 11:27:48 -0500

    Added base for Contact Us form

---
+++ src/components/common/forms/Contact.tsx
@@ -0,0 +1,6 @@
+/* This is a simple 'Contact Us' form that sends the form data to an email service. */
+import styles from './Contact.module.css'
+const Contact = () => {
+
+}
+export default Contact
