5e273c836e4a6167ffed53e07dcd3f6cc55483d9
ruyla <ruyla@proton.me>
2023-08-01 12:28:13 -0500

    Fixed dumb mistake...

--- src/components/common/forms/Contact.tsx
+++ src/components/common/forms/Contact.tsx
@@ -1,6 +1,87 @@
 /* This is a simple 'Contact Us' form that sends the form data to an email service. */
 import styles from './Contact.module.css'
-const Contact = () => {
+import { useState } from 'react'
 
+const Contact = () => {
+    const [values, setValue] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        message: ""
+    })
+    const [errors, setError] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        message: ""
+    })
+
+    const handleChange = () => {
+
+    }
+    const validate = () => {
+
+    }
+    const handleSubmit = () => {
+
+    }
+
+    const title = "Contact Us"
+    const placeholder = "Please let us know your inquiry."
+    return (
+        <div>
+            <form
+                onSubmit={handleSubmit}>
+                <h3>{title}</h3>
+                <div>
+                    <div>
+                        <label
+                            htmlFor="firstName">
+                            First Name
+                        </label>
+                        <input
+                            type="text"
+                            id="firstName"
+                            value={values.firstName}
+                            onChange={handleChange} />
+                        {errors.firstName === "" ? "" : <span>{errors.firstName}</span>}
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="lastName">
+                            Last Name
+                        </label>
+                        <input
+                            type="text"
+                            id="lastName"
+                            value={values.lastName}
+                            onChange={handleChange} />
+                        {errors.lastName === "" ? "" : <span>{errors.lastName}</span>}
+                    </div>
+                </div>
+                <div>
+                    <label
+                        htmlFor="email">
+                        Email Address
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={values.email}
+                        onChange={handleChange} />
+                    {errors.email === "" ? "" : <span>{errors.email}</span>}
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        id="message"
+                        placeholder={placeholder}
+                        value={values.message}
+                        onChange={handleChange} />
+                    {errors.message === "" ? "" : <span>{errors.message}</span>}
+                </div>
+            </form>
+        </div>
+    )
 }
 export default Contact
