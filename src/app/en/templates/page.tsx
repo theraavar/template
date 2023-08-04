9cf05859adf35ae8003c71c709a9c8be4b2942e7
ruyla <ruyla@proton.me>
2023-08-03 20:51:01 -0500

    Who knows what I've done?

--- src/app/en/templates/page.tsx
+++ src/app/en/templates/page.tsx
@@ -1,24 +1,25 @@
-import Slideshow from '@/components/common/slideshows/Slideshow'
 import styles from './Page.module.css'
+import Contact from '@/components/common/forms/Contact'
 
 
 const Page = () => {
     return (
-        <div className="min-h-screen h-full w-full bg-[color:var(--alpha)] px-[5%]">
-            <div className="h-full min-h-screen w-full flex flex-col items-center justify-center min-h-60">
-                <h3 className="text-[color:var(--text)]">Borders & Backgrounds</h3>
+        <div className="min-h-screen flex flex-col items-center justify-center h-full w-full bg-[color:var(--background-light)] px-[5%] pt-12">
+            <div className="h-full min-h-screen w-full flex flex-col items-center justify-around min-h-60">
+                <Contact />
                 <div className="bg-[color:var(--delta)] h-full flex flex-col items-center justify-center min-h-60">
+                    <h3 className="text-[color:var(--black)]">Borders & Backgrounds</h3>
                     <div className="h-full w-full flex items-center justify-center min-h-60">
-                        <span className={`${styles.base} bg-[color:var(--alpha)] border border-[color:var(--beta)]`}>Border 1</span>
-                        <span className={`${styles.base} bg-[color:var(--alpha)] border-2 border-[color:var(--beta)]`}>Border 2</span>
-                        <span className={`${styles.base} bg-[color:var(--alpha)] border-4 border-[color:var(--beta)]`}>Border 4</span>
-                        <span className={`${styles.base} bg-[color:var(--alpha)] border-[6px] border-[color:var(--beta)]`}>Border 6</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--alpha)] border border-[color:var(--beta)]`}>Border 1</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--alpha)] border-2 border-[color:var(--beta)]`}>Border 2</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--alpha)] border-4 border-[color:var(--beta)]`}>Border 4</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--alpha)] border-[6px] border-[color:var(--beta)]`}>Border 6</span>
                     </div>
                     <div className="h-full w-full flex items-center justify-center min-h-60">
-                        <span className={`${styles.base} bg-[color:var(--beta)] border border-[color:var(--alpha)] text-[color:var(--gamma)]`}>Border 1</span>
-                        <span className={`${styles.base} bg-[color:var(--beta)] border-2 border-[color:var(--alpha)] text-[color:var(--delta)] `}>Border 2</span>
-                        <span className={`${styles.base} bg-[color:var(--beta)] border-4 border-[color:var(--alpha)] text-[color:var(--white)]`}>Border 4</span>
-                        <span className={`${styles.base} bg-[color:var(--beta)] border-[6px] border-[color:var(--alpha)]`}>Border 6</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--beta)] border border-[color:var(--alpha)]`}>Border 1</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--beta)] border-2 border-[color:var(--alpha)] `}>Border 2</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--beta)] border-4 border-[color:var(--alpha)]`}>Border 4</span>
+                        <span className={`${styles.base} ${styles.text_white} bg-[color:var(--beta)] border-[6px] border-[color:var(--alpha)]`}>Border 6</span>
                     </div>
                     <div className="h-full w-full flex items-center justify-center min-h-60">
                         <span className={`${styles.base} bg-[color:var(--gamma)] border border-[color:var(--beta)]`}>Border 1</span>
