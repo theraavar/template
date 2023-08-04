2d7abcd2af2b13881ea33cfb192da1cf6cd5fcf6
ruyla <ruyla@proton.me>
2023-08-03 21:18:09 -0500

    Reworking how our Form is going to look

--- src/components/common/forms/Contact.tsx
+++ src/components/common/forms/Contact.tsx
@@ -1,9 +1,20 @@
+//"use client"
+
+import styles from './Contact.module.css'
+//import { useState } from 'react'
+
 const Contact = () => {
 
+    //const [value, setValue] = useState('')
 
     return (
         <form>
-
+            <div className={styles.div}>
+                <label className={styles.label}>
+                    Label
+                </label>
+                <input className={styles.input} />
+            </div>
         </form>
     )
 }
